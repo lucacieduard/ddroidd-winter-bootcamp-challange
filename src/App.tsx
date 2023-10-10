e992af3b39ecc7a168950edac110e723f8dfc770
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-10-10 16:24:27 +0300

    basic routing & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.scss";
 
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Form from "./pages/Form/Form";
+import Success from "./pages/Success/Success";
+import Layout from "./layout/Layout";
+
 function App() {
-  return <div>Start</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="form">
+          <Route index element={<Form />} />
+          <Route path="success" element={<Success />} />
+        </Route>
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
